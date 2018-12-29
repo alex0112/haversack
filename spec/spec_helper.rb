4c02ec9426485741cd9bfb9c85ed25e836f3fbb7
alex0112 <alarsen0112@gmail.com>
2018-12-28 23:53:30 -0700

    spec config to fail fast

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  ## Fail fast
+  config.fail_fast = true
+  
   # Disable RSpec exposing methods globally on `Module` and `main`
   # config.disable_monkey_patching!
 
